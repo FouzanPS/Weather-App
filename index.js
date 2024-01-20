83f86e8cece149dde0233a6325f6edde9b14b0d2
FouzanPS <mohammedfouzanmsd7@gmail.com>
2024-01-21 01:03:37 +0530

    important update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import axios from 'axios';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -10,52 +13,43 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+const openWeatherMapApiKey = process.env.OPENWEATHERMAP_API_KEY;
+
 app.get('/', (req, res) =>{
     res.render('index.ejs');
 });
 
 function kelvinToCelsius(kelvin) {
-    return  kelvin - 273.15;
+    return kelvin - 273.15;
 }
 
 app.get('/weather', async (req, res) => {
     try {
         const place = req.query.place;
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=f639a87a41a0a73d788bc7c50ff92ada`);
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${openWeatherMapApiKey}`);
+        const image = `https://openweathermap.org/img/w/${result.data.weather[0].icon}.png`
         
-        // Check for successful response status
-        if (response.status === 200) {
-            const result = response.data;
-            const image = `https://openweathermap.org/img/w/${result.weather[0].icon}.png`
-            
-            res.render('client.ejs', {
-                temperature: Math.round(kelvinToCelsius(result.main.temp)),
-                city: result.name,
-                lat: result.coord.lat,
-                lon: result.coord.lon,
-                description: result.weather[0].description,
-                wind: result.wind.speed,
-                pressure: result.main.pressure,
-                humidity: result.main.humidity,
-                image: image
-            });
-        } else {
-            console.error(`Error: ${response.status} - ${response.statusText}`);
-            res.status(response.status).render('error.ejs');
-        }
+        res.render('client.ejs', {
+            temperature: Math.round(kelvinToCelsius(result.data.main.temp)),
+            city: result.data.name,
+            lat: result.data.coord.lat,
+            lon: result.data.coord.lon,
+            description: result.data.weather[0].description,
+            wind: result.data.wind.speed,
+            pressure: result.data.main.pressure,
+            humidity: result.data.main.humidity,
+            image: image
+        });
     } catch (error) {
-        console.error("Error:", error.message);
+        if (error.response) {
+            console.error(error.response.data);
+        } else {
+            console.error("Error without a response");
+        }
         res.status(500).render('error.ejs');
     }
 });
 
-
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
